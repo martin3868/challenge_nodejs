031cd3dfe6671ed1df312a17414f44d535e31584
martin3868 <mmila83@hotmail.com>
2021-10-01 22:08:43 -0300

    CRUD

--- controllers/movies.js
+++ controllers/movies.js
@@ -16,6 +16,79 @@ const moviesController = {
         .then(function(movie) {
             res.render("moviesDetail", { movie })
         })
+    },
+    new: (req, res) => {
+        db.Genres.findAll()
+            .then(function(genres) {
+                 res.render("newMovie", { genres })
+            })
+
+    },
+    store: (req, res) => {
+        const movie = {
+            title: req.body.title,
+            rating: req.body.rating,
+            awards: req.body.awards,
+            length: req.body.length,
+            release_date: req.body.release_date,        
+            genre_id:req.body.genre
+        };
+            
+        db.Movies.create(movie)
+            .then((movieCreated) => {
+                res.redirect('/movies/detail/' + movieCreated.id);
+            })
+    },
+
+    edit: async (req, res) => {
+        const { id } = req.params
+
+
+        const movie = await db.Movies.findByPk(id)
+        const genres = await db.Genres.findAll()
+        const actors = await db.Actors.findAll ()
+
+        res.render ('editMovie', {
+            movie,
+            genres,
+            actors
+        });
+    },
+
+    update: async (req, res) => {
+
+       const { id } = req.params
+        const movie = {
+            title: req.body.title,
+            rating: req.body.rating,
+            awards: req.body.awards,
+            length: req.body.length,
+            release_date: req.body.release_date,        
+            genre_id:req.body.genre
+        };
+            
+        db.Movies.update(movie, {
+            where : {
+                id:id
+            }
+        })
+            
+                res.redirect('/movies/detail/' + id);
+            
+    },
+    delete: (req, res) => {
+        const id = req.params.id;
+
+        db.Movies.destroy({
+            where: {
+                id: id
+            }
+        })
+        
+        
+            res.redirect('/');
+        
+        
     }
 
     
--- routes/movies.js
+++ routes/movies.js
@@ -6,4 +6,15 @@ const moviesController = require ("../controllers/movies")
 // Detalle de peliculas //
 router.get('/detail/:id', moviesController.detail);
 
+// Crear Pelicula // 
+router.get('/new', moviesController.new);
+router.post('/new', moviesController.store);
+
+// Editar Pelicula //
+router.get('/edit/:id', moviesController.edit);
+router.post('/edit/:id', moviesController.update);
+
+// Borrar Pelicula //
+router.post('/delete/:id', moviesController.delete)
+
 module.exports = router
