7bcc7ec7f3ca658ad935477b272995f973208837
martin3868 <mmila83@hotmail.com>
2021-10-03 16:11:34 -0300

    Instalacion "bcryptjs" // generacion de vista, ruta y controlador (registro usuario(

---
+++ controllers/users.js
@@ -0,0 +1,11 @@
+    const usersController = {
+        register: (req, res) => {
+            res.render('usersRegister')
+        },
+
+        processRegister: (req, res) => {
+
+        }
+    }
+
+    module.exports = usersController
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
+var usersController = require ("../controllers/users")
+
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+router.get('/register', usersController.register)
+router.post('/register', usersController.processRegister)
 
 module.exports = router;
